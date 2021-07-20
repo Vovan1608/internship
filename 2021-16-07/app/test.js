b6f045d27d39872fdb33d7e559190b1a2b2d892b
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-07-20 14:00:16 +0300

    added task2,3

---
+++ 2021-16-07/app/test.js
@@ -0,0 +1,127 @@
+mocha.setup('bdd');
+let assert = chai.assert;
+
+// test first task
+describe ('render', _ => {
+
+  const test = (...args) => {
+    let expected = '';
+    let [height, width, symbol] = args;
+
+    for (let i = 0; i < height; i += 1) {
+      for (let j = 0; j < width; j += 1) {
+        if (!(i % 2)) {
+          expected += !(j % 2) ? symbol : ' ';
+        } else {
+          expected += !(j % 2) ? ' ' : symbol;
+        }
+        expected += '\n';
+      }
+    }
+
+    it (`шахматная доска с высотой ${height}, шириной ${width} и символом ${symbol}`, _ => {
+      assert.equal(render(...args), expected);
+    });
+
+    it('длина итоговой строки', _ => {
+      assert.equal(render(...args).length, height * width + height - 1);
+    });
+
+    it('выходной параметр - тип srting', _ => {
+      assert.isString(render(...args));
+    });
+  }
+
+  const EXCLAMATION_POINT_IN_ASCII = 33;
+  const AMPERSAND_IN_ASCII = 39;
+  const START_FIRST_PARAM = 4;
+  const END_FIRST_PARAM = 10;
+  const START_SECOND_PARAM = 12;
+  const END_SECOND_PARAM = 18;
+
+  let i = START_FIRST_PARAM;
+  let j = START_SECOND_PARAM;
+  let k = EXCLAMATION_POINT_IN_ASCII;
+
+  for (; i < END_FIRST_PARAM, j < END_SECOND_PARAM, k < AMPERSAND_IN_ASCII; i += 1, j += 1, k += 1) {
+    test(i, j, String.fromCharCode(k));
+  }
+});
+
+const NUM_OF_CASES = 100;
+
+// test second task
+describe ('checkEnvelops', _ => {
+  const getRandom = num =>  Math.random() * num;
+  const MAX_VALUE = 999999;
+
+  for (let i = 0; i < NUM_OF_CASES; i += 1) {
+    let [a, b, c, d] = [0, 0, 0, 0].map(el => el = getRandom(MAX_VALUE));
+
+    if (a < c && b < d || a < d && b < c) {
+      it(`конверт1 можно вложить в конверт2`, _ => {
+        assert.equal(checkEnvelops({a, b}, {c, d}), 1);
+      });
+    }
+
+    if (a > c && b > d || a > d && b > c) {
+      it(`конверт2 можно вложить в конверт1`, _ => {
+        assert.equal(checkEnvelops({a, b}, {c, d}), 2);
+      });
+    }
+
+    if (a === c && b === d || a === d && b === c) {
+      it(`конверты нельзя вложить друг в друга`, _ => {
+        assert.equal(checkEnvelops({a, b}, {c, d}), 0);
+      });
+    }
+  }
+});
+
+// test third task
+describe ('sortingTriangles', _ => {
+  const getArea = obj => {
+    const {a, b, c} = obj;
+    const halfP = (a + b + c) / 2;
+
+    return Math.sqrt(halfP * (halfP - a) * (halfP - b) * (halfP - c));
+  }
+
+  const randomInteger = (min, max) => min + Math.random() * (max + 1 - min);
+
+  const makeAnArrOfObj = (size, rangeOfSide) => {
+    let arr = [];
+    const A_IN_ASCII = 65;
+    const Z_IN_ASCII = 90;
+    const NUM_OF_VERTICES = 3;
+
+    for (let i = 0; i < size; i += 1) {
+      let obj = {
+        vertices: `${String.fromCharCode(randomInteger(A_IN_ASCII, Z_IN_ASCII))}`.repeat(NUM_OF_VERTICES),
+        a: randomInteger(0, rangeOfSide),
+        b: randomInteger(0, rangeOfSide),
+        c: randomInteger(0, rangeOfSide)
+      }
+
+      arr.push(obj);
+    }
+
+    return arr;
+  }
+
+  for (let i = 0; i < NUM_OF_CASES; i += 1) {
+    const size = 50;
+    const rangeOfSide = 10;
+    let arr = makeAnArrOfObj(size, rangeOfSide);
+
+    it('упорядоченный массив имён треугольников', _ => {
+      assert.equal(sortingTriangles(arr),
+        arr
+          .sort((cur, next) => getArea(next) - getArea(cur))
+          .map(el => el.vertices)
+      );
+    });
+  }
+});
+
+mocha.run();
