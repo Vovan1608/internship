da1663f21ea576b909af278365219391091864c2
Volodymyr Zenin <vovan1608@gmail.com>
2021-07-28 19:41:43 +0300

    added conditions in task 2

--- 2021-28-07/src/task2.js
+++ 2021-28-07/src/task2.js
@@ -1,18 +1,24 @@
 
 const checkTicTacToe = arr => {
-	const POSSIBLE_NUM_OF_ELEM = 3;
+	const NUM_OF_ELEM = 3;
 
 	try {
-		if (Array.isArray(arr) && arr.length === POSSIBLE_NUM_OF_ELEM) {
-			
+		if (Array.isArray(arr) && arr.length === NUM_OF_ELEM) {
+			if (!arr.flat().every(el => Boolean(el))) {
+				return 0;
+			}
+
+			if (arr.flat().some(el => !Boolean(el))) {
+				return -1;
+			}
 		} else {
 			throw new Error('arr should be array ahd has length 3');
 		}
 	} catch(e) {
-		console.log(e.message);
+		console.error(e.message);
 	}
 
 }
 
-const res = checkTicTacToe('[[0,0,1], [0,2,0], [0,0,0]]');
+const res = checkTicTacToe([[0,1,0], [0,0,2], [0,0,0]]);
 console.log(res);
