e0b8ab2ec6f5496a130b5a47b46326d9975f85d7
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-07-30 22:08:32 +0300

    remove map in HW_4 T_1

--- 2021-29-07/src/task1.js
+++ 2021-29-07/src/task1.js
@@ -3,18 +3,22 @@ import { isIPv4 } from './helpers.js';
 const convertIPV4ToDecimal = ipv4 => {
 	const RADIX = 2;
 	const BITS = 8;
+
 	const res = ipv4
 		.split('.')
-		.map(oktet => Number(oktet).toString(RADIX).padStart(BITS, '0'))
-		.reduce((acc, cur) => acc + cur, '');
+		.reduce((acc, oktet) => {
+			return acc + Number(oktet).toString(RADIX).padStart(BITS, '0');
+		}, 0);
 
 	return parseInt(res, RADIX);
 }
 
 const countIpAdresses = (ipStart, ipEnd) => {
 	if (isIPv4(ipStart) && isIPv4(ipEnd)) {
-		return convertIPV4ToDecimal(ipEnd) - convertIPV4ToDecimal(ipStart);
+		return Math.abs(convertIPV4ToDecimal(ipEnd) - convertIPV4ToDecimal(ipStart));
 	} else {
 		return 'wrong IPv4';
 	}
 }
+
+// console.log(countIpAdresses("10.0.0.0", "10.0.0.50"));
