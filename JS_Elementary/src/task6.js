c2defef11db19dce299777dacb36d2b988cc7d91
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-08-01 18:13:23 +0300

    added task6 JS_E

---
+++ JS_Elementary/src/task6.js
@@ -0,0 +1,12 @@
+const getNumericalSiquence = (length, minSquareVal) => {
+	// let temp = [];
+
+	// for (let i = 0; i < length; i += 1) {
+	// 	if (i * i >= minSquareVal) {
+	// 		temp.push(i);
+	// 	}
+	// }
+	return ;
+}
+
+console.log(getNumericalSiquence(20, 90));
