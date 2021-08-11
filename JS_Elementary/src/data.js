4309dcbbedabe0f34331b33f1614d4f7ee968700
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-08-11 11:39:12 +0300

    added data.js

---
+++ JS_Elementary/src/data.js
@@ -0,0 +1,8 @@
+export const triangels = [
+	{vertices: 'ABC', a: 10, b: 20, c: 22.5},
+	{vertices: 'FGH', f: 7, g: 3.9, h: 8},
+	{vertices: 'QWE', q: 25, w: 20.7, e: 10},
+	{vertices: 'RTY', r: 4, t: 6.8, y: 5},
+	{vertices: 'POK', p: 14, o: 28, k: 21.4},
+	{vertices: 'JKL', j: 18, k: 22.3, l: 25.1},
+]
